3f5bed8cf34f5d1065d0afd765e1ff60acc24cea
Blackout358 <bence.gellert3@gmail.com>
2024-05-31 15:41:42 +0100

    Rewrote from actix_web to axum

--- shopping_list_backend/src/main.rs
+++ shopping_list_backend/src/main.rs
@@ -1,187 +1,139 @@
-// use std::{
-//     io::{prelude::*, BufReader},
-//     net::{TcpListener, TcpStream},
-// };
-// use futures::TryStreamExt;
-// use mongodb::{bson::{doc, Document}, options::{ClientOptions, Collation}, Client, Collection, Database};
-// use bson::{bson, Bson};
-// use serde_json::json;
+use std::{collections, os::unix::net::SocketAddr, str::FromStr};
 
-// #[tokio::main]
-// async fn main() {
-//     let listener = TcpListener::bind("172.19.1.128:7878").unwrap();
-//     let mut item_collection: Collection<Document> = connect_to_db().await.unwrap();
-//     // let uri = "mongodb://localhost:27017";
-//     // let db = connect_to_db();
-//     // let client = Client::with_uri_str(uri).await?;
-//     // let db = client.database("shopping_list");
-//     // let coll:Collection<Document> = db.collection("items");
-//     // let res = coll.find
-//     // let mut client_options = ClientOptions::parse("mongodb://localhost:27017").await?;
-    
-    
-//     for stream in listener.incoming() {
-//         // connect_to_db();
-//         let stream = stream.unwrap();
-
-//         let _res = handle_connection(stream).await;
-//     }
-// }
-
-
-
-
-// async fn handle_connection(mut stream: TcpStream) {
-//     let buf_reader = BufReader::new(&mut stream);
-//     let request_line = buf_reader.lines().next().unwrap().unwrap();
-//     println!("{request_line}");
-
-//     match request_line {
-//         "GET / HTTP/1.1". => println!("asd")
-//         _ => println!("error")
-//     }
-//     if request_line == "GET / HTTP/1.1" {
-    //         let _res = connect_to_db().await.unwrap();
-    //         println!("{:#?}", _res);
-    //         let status_line = "HTTP/1.1 200 OK"; 
-    //         let contents = serde_json::to_string_pretty(&_res).unwrap();
-//         let length = contents.len();
-
-//         let response = format!(
-    //             "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"
-    //         ); 
-    
-    //         stream.write_all(response.as_bytes()).unwrap();
-    //     } else {
-        //         // some other request
-        //     }
-        // }
-        
-        // // fn handle_connection(mut stream: TcpStream) {
-            // //     let buf_reader = BufReader::new(&mut stream);
-// //     let http_request: Vec<_> = buf_reader
-// //         .lines()
-// //         .map(|result| result.unwrap())
-// //         .take_while(|line| !line.is_empty())
-// //         .collect();
-
-// //     println!("Request: {:#?}", http_request);
-// //     let response  = "Hello";
-// //     stream.write_all(response.as_bytes());
-// // }
-
-
-use std::{str::FromStr, sync::Mutex};
-
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use actix_web::App;
+// use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use axum::{extract::{Path, State}, response::IntoResponse, routing::{get, post}, Extension, Router};
 use bson::{doc, oid::ObjectId, Document};
 use futures::TryStreamExt;
 use mongodb::{Client, Collection};
+use tokio::sync::Mutex;
 
+#[derive(Clone)]
 struct AppState {
-    collection: Mutex<Collection<Document>>, // You can store any type, String is just an example
+    collection: Collection<Document>,
 }
- 
-
-// struct QueryPat
 
 // impl AppState {
-//     fn new() -> Self {}
+//     pub fn new(mut self, value: Collection<Document>) -> Self {
+//         self.collection = value;
+//         self
+//     }
 // }
-
-// Handler functions
-async fn create_item(share_collection: web::Data<AppState>, path: web::Path<String>) -> impl Responder {
-    
-    // let new_item = Item::new(path);
-    let new_item = doc! {"name": path.to_string()};
-    let data = share_collection.collection.lock().unwrap();
-    
+  
+// async fn old_create_item(share_collection: web::Data<AppState>, path: web::Path<String>) -> impl Responder { 
+//     let new_item = doc! {"name": path.to_string()};
+//     let data = share_collection.collection.lock().unwrap();
+//     let res = data.insert_one(new_item, None).await.unwrap();
+//     let res_string = format!("{:#?}", res);
+//     HttpResponse::Ok().body(res_string)
+// }
+ 
+async fn create_item(Path(id): Path<String>, State(state): State<AppState>) -> String{ 
+    let new_item = doc! {"name": id};
+    let data = state.collection.clone();
+    // let data = share_collection.lock().unwrap();
+    // let data = share_collection.collection.lock().unwrap();
     let res = data.insert_one(new_item, None).await.unwrap();
-    let res_string = format!("{:#?}", res);
-    HttpResponse::Ok().body(res_string)
+    format!("{:#?}", res)
+    // HttpResponse::Ok().body(res_string)
 }
 
-async fn read_item(share_collection: web::Data<AppState>) -> impl Responder {
+async fn read_item(State(state): State<AppState>) -> String {
     println!("Inside GET call");
-    let data = share_collection.collection.lock().unwrap();
-    let res = data.find(doc!{}, None).await.unwrap();
-
-
-    
+    // let data = share_collection.collection.lock().unwrap();
+    let coll = state.collection.clone();
+    let res = coll.find(doc!{}, None).await.unwrap();
     let result_vector:Vec<String> = Vec::new();
     let contents = parse_document(res, result_vector).await;
-    HttpResponse::Ok().body(contents)
+    // let contents = String::from("asd");
+    // HttpResponse::Ok().body(contents)
+    contents
 }
 
 async fn parse_document(mut res: mongodb::Cursor<Document>, mut result_vector: Vec<String>) -> String {
     while let Ok(Some(doc)) = res.try_next().await {
-        let res_asd = format!("{:#?}", doc);
-        // let json_string = serde_json::to_string(&doc).unwrap(); 
-        result_vector.push(res_asd); 
+        let res_str = format!("{:#?}", doc); 
+        result_vector.push(res_str); 
     }
-
-    let contents = result_vector.join("\n");
-
-    
-    // let  contents = serde_json::to_string(&result_vector).unwrap();
-    // contents.replace("\\", "")
-    // let res_string = format!("{:#?}", contents);
-    // res_string
+    let contents = result_vector.join("\n"); 
     contents
-    
 }
 
-async fn update_item() -> impl Responder {
-    HttpResponse::Ok().body("Update item")
-}
+// async fn update_item() -> impl Responder {
+//     HttpResponse::Ok().body("Update item")
+// }
 
-async fn delete_item(share_collection: web::Data<AppState>, path: web::Path<String>) -> impl Responder {
+async fn delete_item(Path(id): Path<String>, State(state): State<AppState>) -> String {
     println!("Inside DELETE call");
-    let data = share_collection.collection.lock().unwrap();
-    println!("Passed in value: {}", &path);
+    // let data = share_collection.collection.lock().unwrap();
+    let data = state.collection.clone();
+    println!("Passed in value: {}", &id);
 
     
     let mut _contents= String::new();
-    if let Ok(document_id)  = ObjectId::from_str(&path.to_string()){
-
-        // let res = data.find(doc! {"_id": document_id}, None).await.unwrap();
-        let delete_result = data.delete_one(doc!{"_id": document_id},None).await.unwrap();
-        // let result_vector = Vec::new();
-        // contents = parse_document(res, result_vector).await;
-        // println!("{}", parsedelete_result);
+    if let Ok(document_id)  = ObjectId::from_str(&id.to_string()){ 
+        let delete_result = data.delete_one(doc!{"_id": document_id},None).await.unwrap(); 
         _contents = format!("{:#?}", delete_result);
     
     } else {
         _contents = "Invalid Document ID".to_string();
     }
     println!("{_contents}");
-    
-    HttpResponse::Ok().body(format!("{}\n", _contents))
-    // println!("{res}");
+    _contents
+    // HttpResponse::Ok().body(format!("{}\n", _contents)) 
 }
 
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
+// #[actix_web::main]
+// async fn main() -> std::io::Result<()> {
+//     let item_collection:Collection<Document> = connect_to_db().await.unwrap();
+
+//     let share_collection = web::Data::new(AppState {
+//         collection: Mutex::new(item_collection),
+//     });
+//     HttpServer::new(move || {
+//         App::new()
+//         .app_data(share_collection.clone())
+//         // Create
+//         .route("/items/{id}", web::post().to(create_item))
+//         // Read
+//         .route("/items/", web::get().to(read_item))
+//         // Update
+//         .route("/items/{id}", web::put().to(update_item))
+//         // Delete
+//         .route("/items/{id}", web::delete().to(delete_item))
+//     })
+//     .bind("172.19.1.128:7878")?
+//     .run()
+//     .await
+// }
+#[tokio::main]
+async fn main() {
     let item_collection:Collection<Document> = connect_to_db().await.unwrap();
 
-    let share_collection = web::Data::new(AppState {
-        collection: Mutex::new(item_collection),
-    });
-    HttpServer::new(move || {
-        App::new()
-        .app_data(share_collection.clone())
-        // Create
-        .route("/items/{id}", web::post().to(create_item))
-        // Read
-        .route("/items", web::get().to(read_item))
-        // Update
-        .route("/items/{id}", web::put().to(update_item))
-        // Delete
-        .route("/items/{id}", web::delete().to(delete_item))
-    })
-    .bind("172.19.1.128:7878")?
-    .run()
-    .await
+    let state = AppState {collection: item_collection.clone()};
+
+    // let share_collection = web::Data::new(AppState {
+    //     collection: Mutex::new(item_collection),
+    // });
+
+    // let share_collection: Mutex<Collection<Document>> = Mutex::new(item_collection);
+    // let share_collection = AppState {collection: Mutex::new(item_collection)};
+    let app = Router::new()
+    .route("/items/", get(read_item))
+    
+    .route("/items/:id", post(create_item).delete(delete_item))
+    // .layer(Extension(share_collection));
+    .with_state(state);
+
+    // let addr = SocketAddr::from(([172,19,1,128], 7878));
+    // println!("Listening on {:?}", &addr);
+    // axum::Server::bind(&addr).serve
+    let listener = tokio::net::TcpListener::bind("172.19.1.128:7878").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn handler() -> String {
+    "Hello, world".to_string()
 }
 
 async fn connect_to_db() -> mongodb::error::Result<Collection<Document>>  {
@@ -189,17 +141,6 @@ async fn connect_to_db() -> mongodb::error::Result<Collection<Document>>  {
     let uri = "mongodb://localhost:27017";
     let client = Client::with_uri_str(uri).await?;
     let db = client.database("shopping_list");
-   let item_collection = db.collection("items");
-    // let mut res = item_collection.find(doc!{}, None).await?;
-    // let mut result_vector:Vec<String> = Vec::new();
-    // while let Some(doc) = res.try_next().await? {
-    //     let json_string = serde_json::to_string(&doc).unwrap();
-
-    //     // let string:Bson = doc.clone().into();
-    //     result_vector.push(json_string);
-    //     // result_vector.push(json_string.clone());
-    //     // println!("{}", json_string);
-    // }
-    // Ok(result_vector)
+   let item_collection = db.collection("items"); 
     Ok(item_collection)
 }
