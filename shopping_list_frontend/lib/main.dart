707b15f0aebe55527a3ffe103cca4ed9bd5f868f
Blackout358 <bence.gellert3@gmail.com>
2024-06-03 13:45:52 +0100

    Proper structure

--- shopping_list_frontend/lib/main.dart
+++ shopping_list_frontend/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_list_frontend/Screens/main_page.dart';
 import 'package:shopping_list_frontend/src/rust/api/communication.dart';
 import 'package:shopping_list_frontend/src/rust/api/item_model.dart';
 import 'package:shopping_list_frontend/src/rust/api/simple.dart';
@@ -17,22 +18,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: const Text('flutter_rust_bridge quickstart')),
-        body: Center(
-          child: FutureBuilder<List<Item>>(
-              future: getItems(),
-              builder: (context, snapshot) {
-                // Check if the future has completed
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return CircularProgressIndicator(); // Show loading indicator
-                } else if (snapshot.hasError) {
-                  return Text('Error: ${snapshot.error}'); // Show error message
-                } else {
-                  // Show data from the future
-                  return Text('Data: ${snapshot.data}');
-                }
-              }),
-        ),
+        appBar: AppBar(
+            centerTitle: true,
+            title: const Text(
+              'Shopping List',
+            )),
+        body: const MainPage(),
       ),
     );
   }
