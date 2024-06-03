58ef7da13777d2c17b6ed7a61e9ef8bb1d187431
Blackout358 <bence.gellert3@gmail.com>
2024-06-03 13:46:39 +0100

    Cleaned up comments

--- shopping_list_frontend/lib/Widgets/item_card.dart
+++ shopping_list_frontend/lib/Widgets/item_card.dart
@@ -15,17 +15,8 @@ class ItemCard extends StatelessWidget {
         title: Text(item.name,
             style: const TextStyle(fontWeight: FontWeight.bold)),
         subtitle: Text(item.id.oid),
-        // subtitle: const Column(
-        //   crossAxisAlignment: CrossAxisAlignment.start,
-        //   children: <Widget>[
-        //     // Text('Muscles Worked: ${muscleWorked.join(', ')}'),
-        //     // Text(
-        //     //     'Equipment Used: ${equipmentUsed.isNotEmpty ? equipmentUsed.join(', ') : 'None'}'),
-        //   ],
-        // ),
         trailing: IconButton(
           icon: const Icon(Icons.delete),
-          // icon: Icon(Icons.info_outline),
           onPressed: () {},
         ),
       ),
